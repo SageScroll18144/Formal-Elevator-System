0fd7f90e5e3ada55af2a548790b83dfe4fc1b1ca
Mend25 <113634496+Mend25@users.noreply.github.com>
2024-07-03 15:47:25 -0300

    Operações de abrir e fechar a porta implementadas

--- PJ2/elevator_system.js
+++ PJ2/elevator_system.js
@@ -50,37 +50,46 @@ for (var i = 1; i <= 8; i++){
     }
   });
 
-  // Portas do pavimento
-  bms.observe('formula', {
-    selector: "#floor" + i, 
-    formulas: ["status_door", "current_floor"],
-    trigger: function (origin, values) { 
-      if(values[0] === "TRUE" && values[1] === i.toString()) {
+  //função de atualização da porta
+  (function(index) {
+    function updateDoor(origin, values) {
+      console.log("Valor de index: ", index);
+      if (values[0] === "TRUE" && values[1] === index.toString()) {
         origin.attr("xlink:href", "open_door.png");
       } else {
         origin.attr("xlink:href", "close_door.png");
-      }      
+      }
     }
-  });
+  
+  
+    // Portas do pavimento
+    bms.observe('formula', {
+      selector: "#floor" + i, 
+      formulas: ["status_door", "current_floor"],
+      trigger: function (origin, values) { 
+         updateDoor(origin, values);      
+      }
+    });
+  })(i);
 
   // Display do pavimento(sentido vetorial do elevador)
-  bms.observe('formula', {
-    selector: "#display_floor_vector" + i, 
-    formulas: ["current_direction"],
-    trigger: function (origin, values) {
-      switch (values[0]) {
-        case "up":
-          origin.attr("xlink:href", "up.png");
-          break;
-        case "down":
-          origin.attr("xlink:href", "down.png");
-          break;
-        case "empty":
-          origin.attr("xlink:href", "none.png");
-          break;
-      }       
-    }
-  });
+   bms.observe('formula', {
+     selector: "#display_floor_vector" + i, 
+     formulas: ["current_direction"],
+     trigger: function (origin, values) {
+       switch (values[0]) {
+         case "up":
+           origin.attr("xlink:href", "up.png");
+           break;
+         case "down":
+           origin.attr("xlink:href", "down.png");
+           break;
+         case "empty":
+           origin.attr("xlink:href", "none.png");
+           break;
+       }       
+     }
+   });
 
   // Botões do pavimento: UP
   bms.observe('formula', {
