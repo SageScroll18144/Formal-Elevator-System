8b273e533422106f33b1790611fee073ab9ac25b
SageScroll18144 <ffss@cin.ufpe.br>
2024-07-03 09:39:11 -0300

    FIX: condição da porta

--- PJ2/elevator_system.js
+++ PJ2/elevator_system.js
@@ -57,7 +57,7 @@ for (var i = 1; i <= 8; i++){
     selector: "#floor" + i, 
     formulas: ["status_door", "current_floor"],
     trigger: function (origin, values) { 
-      if(values[0] == "TRUE" && values[1] == i) {
+      if(values[0] === "TRUE" && values[1] === i.toString()) {
         origin.attr("xlink:href", "open_door.png");
       } else {
         origin.attr("xlink:href", "close_door.png");
