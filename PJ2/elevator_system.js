75c913dd5242c56d58deda695ed173b82565bbcb
SageScroll18144 <ffss@cin.ufpe.br>
2024-07-03 00:35:53 -0300

    FEAT: Implementação de todos os observadores

--- PJ2/elevator_system.js
+++ PJ2/elevator_system.js
@@ -1,47 +1,10 @@
-// for (var i = 1; i <= 7; i++) {
-
-//   bms.observe('formula', {
-//     selector: "#pos" + i,
-//     formulas: ["positions(" + i + ")"],
-//     trigger: function(origin, res) {
-//       origin.attr("xlink:href", res[0].substring(0, 5) + ".png");
-//       origin.attr("data-frog", res[0]);
-//     }
-//   });
-
-//   // bms.executeEvent({
-//   //   selector: "#pos" + i,
-//   //   events: [{
-//   //     name: "Move_right",
-//   //     predicate: function(origin) {
-//   //       return "x=" + origin.attr("data-frog");
-//   //     }
-//   //   }, {
-//   //     name: "Move_left",
-//   //     predicate: function(origin) {
-//   //       return "x=" + origin.attr("data-frog");
-//   //     }
-//   //   }, {
-//   //     name: "Hop_right",
-//   //     predicate: function(origin) {
-//   //       return "x=" + origin.attr("data-frog");
-//   //     }
-//   //   }, {
-//   //     name: "Hop_left",
-//   //     predicate: function(origin) {
-//   //       return "x=" + origin.attr("data-frog");
-//   //     }
-//   //   }]
-//   // });
-
-// }
-
 for (var i = 1; i <= 8; i++){
+
+  // Botões internos do elevador
   bms.observe('formula', {
     selector: "#btn_red" + i, 
     formulas: ["btn_actions(" + i + ")"],
     trigger: function (origin, values) {
-      
       switch (values[0]) {
         case "FALSE":
           origin.attr("xlink:href", "btn_red.png");
@@ -53,11 +16,11 @@ for (var i = 1; i <= 8; i++){
     }
   });
 
+  // Display 7 segmentos interno do elevador(visor)
   bms.observe('formula', {
     selector: "#display_floor_id", 
     formulas: ["display_floor(" + i + ")"],
     trigger: function (origin, values) {
-      
       if(values[0] == "TRUE") {
         switch (i) {
           case 1:
@@ -85,9 +48,68 @@ for (var i = 1; i <= 8; i++){
             origin.attr("xlink:href", "eight.png");
             break;
         }  
-      }
-
-          
+      }           
     }
   });
+
+  // Portas do pavimento
+  bms.observe('formula', {
+    selector: "#floor" + i, 
+    formulas: ["display_floor(" + i + ")"],
+    trigger: function (origin, values) {
+      if(values[0] == "TRUE") {
+        origin.attr("xlink:href", "open_door.png");
+      } else {
+        origin.attr("xlink:href", "close_door.png");
+      }      
+    }
+  });
+
+  // Display do pavimento(sentido vetorial do elevador)
+  bms.observe('formula', {
+    selector: "#display_floor_vector" + i, 
+    formulas: ["current_direction"],
+    trigger: function (origin, values) {
+      switch (values[0]) {
+        case "up":
+          origin.attr("xlink:href", "up.png");
+          break;
+        case "down":
+          origin.attr("xlink:href", "down.png");
+          break;
+        case "empty":
+          origin.attr("xlink:href", "none.png");
+          break;
+      }       
+    }
+  });
+
+  // Botões do pavimento: UP
+  bms.observe('formula', {
+    selector: "#btn_floor" + (2 * i), 
+    formulas: ["floor_leds(" + i + ")"],
+    trigger: function (origin, values) {
+      if(values[0] == "up"){
+        origin.attr("xlink:href", "btn_green.png");
+      }else {
+        origin.attr("xlink:href", "btn_red.png");
+      }
+
+    }
+  });
+
+  // Botões do pavimento: DOWN
+  bms.observe('formula', {
+    selector: "#btn_floor" + (2 * i - 1),
+    formulas: ["floor_leds(" + i + ")"],
+    trigger: function (origin, values) {
+      if(values[0] == "down"){
+        origin.attr("xlink:href", "btn_green.png");
+      }else {
+        origin.attr("xlink:href", "btn_red.png");
+      }
+
+    }
+  });
+
 }
