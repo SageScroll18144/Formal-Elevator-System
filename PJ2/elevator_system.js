e590b7a1964c4c9263e668e35eb511a41947f1e9
Mend25 <113634496+Mend25@users.noreply.github.com>
2024-07-03 22:37:53 -0300

    Debugging

--- PJ2/elevator_system.js
+++ PJ2/elevator_system.js
@@ -96,6 +96,7 @@ for (var i = 1; i <= 8; i++){
     formulas: ["floor_leds(" + i + ")"],
     trigger: function (origin, values) {
       console.log("tamanho do vetor values: ", values[0].length);
+      console.log("Vetor values: ", values[0]);
       if(values[0].length > 0){
        (function(index) {
           if(index <= values[0].length && values[0][index-1] === "up"){
@@ -114,7 +115,7 @@ for (var i = 1; i <= 8; i++){
    bms.observe('formula', {
      selector: "#btn_floor" + ((2 * i) - 1),
      formulas: ["floor_leds(" + i + ")"],
-     trigger: function (origin, values) {
+     trigger: function (origin, values) { 
        if(values[0].length > 0){
          (function(index){
            if(index <= values[0].length && values[0][index-1] === "down"){
