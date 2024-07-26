043b55186f4397fb2d2aa712808c18a4fc3f087c
SageScroll18144 <ffss@cin.ufpe.br>
2024-07-25 23:19:24 -0300

    FIX: correções na implementação e na geração de codigo

---
+++ PJ3/c/Elevator_System.h
@@ -0,0 +1,95 @@
+#ifndef _Elevator_System_h
+#define _Elevator_System_h
+
+#include <stdint.h>
+#include <stdbool.h>
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+
+/* Clause SETS */
+typedef enum
+{
+    Elevator_System__up,
+    Elevator_System__down,
+    Elevator_System__empty
+    
+} Elevator_System__ELEVATOR_DIRECTIONS;
+#define Elevator_System__ELEVATOR_DIRECTIONS__max 3
+typedef enum
+{
+    Elevator_System__btn_alarm,
+    Elevator_System__btn_phone,
+    Elevator_System__btn_open,
+    Elevator_System__btn_close
+    
+} Elevator_System__ELEVATOR_SPECIAL_BUTTONS;
+#define Elevator_System__ELEVATOR_SPECIAL_BUTTONS__max 4
+typedef enum
+{
+    Elevator_System__btn_up,
+    Elevator_System__btn_down
+    
+} Elevator_System__FLOOR_BUTTONS;
+#define Elevator_System__FLOOR_BUTTONS__max 2
+
+/* Clause CONCRETE_CONSTANTS */
+/* Basic constants */
+#define Elevator_System__NUM_FLOOR 8
+#define Elevator_System__NUM_ELEVATOR 1
+typedef int Elevator_System__ELEVATOR_ORDINARY_BUTTONS;
+typedef int Elevator_System__ELEVATOR_FLOOR_DISPLAY;
+/* Array and record constants */
+
+
+
+
+
+/* Clause CONCRETE_VARIABLES */
+
+extern void Elevator_System__INITIALISATION(void);
+
+/* Clause OPERATIONS */
+
+extern void Elevator_System__move(void);
+extern void Elevator_System__elevator_operation_request(int32_t user_orders);
+extern void Elevator_System__elevator_operation_alarm(void);
+extern void Elevator_System__elevator_operation_report(void);
+extern void Elevator_System__elevator_operation_hold_door(void);
+extern void Elevator_System__elevator_operation_open_door(void);
+extern void Elevator_System__elevator_operation_close_door(void);
+extern void Elevator_System__elevator_operation_let_go(void);
+extern void Elevator_System__elevator_operation_break_to_repair(void);
+extern void Elevator_System__elevator_operation_repaired(void);
+extern void Elevator_System__enter_elevator(int32_t weight);
+extern void Elevator_System__exit_elevator(void);
+extern void Elevator_System__get_phone_call_from_elevator(void);
+extern void Elevator_System__intermediary_call_elevator(int32_t number_floor, Elevator_System__ELEVATOR_DIRECTIONS direction);
+extern void Elevator_System__ground_floor_call_elevator(void);
+extern void Elevator_System__last_floor_call_elevator(void);
+extern void Elevator_System__cancel(int32_t num_floor);
+extern void Elevator_System__cancel_floor(int32_t num_floor, Elevator_System__ELEVATOR_DIRECTIONS direction);
+
+void print_alarm_door();
+void print_ask_to_repair();
+void print_behavior();
+void print_btn_actions();
+void print_btn_special();
+void print_cancel();
+void print_current();
+void print_display_directon();
+void print_display_floor();
+void print_people_in_elev();
+void print_ptr_qtd();
+void print_sensors();
+void print_state_blink();
+void print_status();
+void print_time();
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+
+#endif /* _Elevator_System_h */
---
+++ PJ3/c/main.c
@@ -0,0 +1,142 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include "Elevator_System.h"
+
+int main() {
+
+    Elevator_System__INITIALISATION();
+
+    int flag = 1, w, d; 
+
+    while(flag) {
+        printf("Escolha uma operação: ");
+        printf("\n\t - 1) Move\n\t - 2) Requisicao Interna do Elevator\n\t - 3) Alarme\n\t - 4) Reportar\n\t - 5) Segurar a porta\n\t - 6) Abrir a porta\n\t - 7) Fechar porta\n\t - 8) LetGo(Tirar mão da porta)\n\t - 9) Parar elevador para reparo\n\t - 10) Reparar\n\t - 11) Entrar no elevador\n\t - 12) Sair do elevador\n\t - 13) Portaria atender interfone\n\t - 14) Chamada externa de pavimento\n\t - 15) Cancelamento interno\n\t - 16) Cancelamento externo\n");
+        printf(".: ");
+
+        int op;
+        scanf("%d", &op);
+
+        switch (op) {
+        case 1:
+            Elevator_System__move();
+            break;
+        
+        case 2:
+            printf(" - Informe o andar que você deseja: "); 
+            w;
+            scanf("%d", &w);
+            Elevator_System__elevator_operation_request(w);
+            break;
+
+        case 3:
+            Elevator_System__elevator_operation_alarm();
+            break;
+
+        case 4:
+            Elevator_System__elevator_operation_report();
+            break;
+
+        case 5:
+            Elevator_System__elevator_operation_hold_door();
+            break;
+
+        case 6:
+            Elevator_System__elevator_operation_open_door();
+            break;
+
+        case 7:
+            Elevator_System__elevator_operation_close_door();
+            break;
+
+        case 8:
+            Elevator_System__elevator_operation_let_go();
+            break;
+
+        case 9:
+            Elevator_System__elevator_operation_break_to_repair();
+            break;
+
+        case 10:
+            Elevator_System__elevator_operation_repaired();
+            break;
+        
+        case 11:
+            printf(" - Informe o seu peso: "); 
+            w;
+            scanf("%d", &w);
+            Elevator_System__enter_elevator(w);
+            break;
+
+        case 12:
+            Elevator_System__exit_elevator();
+            break;
+        
+        case 13:
+            Elevator_System__get_phone_call_from_elevator();
+            break;
+        
+        case 14:
+            w;
+            printf(" - Informe o andar: ");
+            scanf("%d", &w);
+            if(w >= 7) {
+                Elevator_System__last_floor_call_elevator();
+            }
+            else if(w == 1) {
+                Elevator_System__ground_floor_call_elevator();
+            } else {
+                d;
+                printf(" - Informe a direção | 1 -> up | 0 -> down: ");
+                scanf("%d", &d);
+
+                if(d) {
+                    Elevator_System__intermediary_call_elevator(w, Elevator_System__up);
+                } else {
+                    Elevator_System__intermediary_call_elevator(w, Elevator_System__down);
+                }
+            }
+
+            break;
+
+        case 15:
+            w;
+            printf(" - Informe o andar: ");
+            scanf("%d", &w);
+            Elevator_System__cancel(w);
+            break;
+
+        case 16:
+            w;
+            printf(" - Informe o andar: ");
+            scanf("%d", &w);
+            d;
+            printf(" - Informe a direção | 1 -> up | 0 -> down: ");
+            scanf("%d", &d);
+            Elevator_System__cancel_floor(w, d);
+            break;    
+
+        default:
+            flag = 0;
+        }
+
+        // Historico
+        print_alarm_door();
+        print_ask_to_repair();
+        print_behavior();
+        print_btn_actions();
+        print_btn_special();
+        print_cancel();
+        print_current();
+        print_display_directon();
+        print_display_floor();
+        print_people_in_elev();
+        print_ptr_qtd();
+        print_sensors();
+        print_state_blink();
+        print_status();
+        print_time();
+        
+    }
+
+    return 0;
+}
